f72c64fd543cec54df826463935562c575662cef
Jelle Z <jellezwiers@icloud.com>
2019-07-26 13:28:42 +0200

    Improved quality of JavaScript code in script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  function OnInput() {
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
+  }
   var tx = document.getElementsByTagName("textarea");
   for (var i = 0; i < tx.length; i++) {
     tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
     tx[i].addEventListener("input", OnInput, false);
   }
-
-  function OnInput() {
-    this.style.height = "auto";
-    this.style.height = (this.scrollHeight) + "px";
-  }
 });
 
-var currentTimeZone = luxon.DateTime.fromObject({zone: 'Europe/Amsterdam'}).toLocaleString(luxon.DateTime.DATETIME_FULL);
+var currentTimeZone = luxon.DateTime.fromObject({zone: "Europe/Amsterdam"}).toLocaleString(luxon.DateTime.DATETIME_FULL);
