823f0e65b5a5368b5e35c2e053c22c9c16c11878
Jelle Z <jellezwiers@icloud.com>
2019-08-17 15:57:53 +0200

    Added Netlify CMS and updated build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#            _       _                
+#  _ __ ___ | | ____| | ___   ___ ___ 
+# | '_ ` _ \| |/ / _` |/ _ \ / __/ __|
+# | | | | | |   < (_| | (_) | (__\__ \
+# |_| |_| |_|_|\_\__,_|\___/ \___|___/
+                                     
 mkdir characters
 mkdocs build --clean --config-file mkdocs/characters/mkdocs.yml --site-dir ../../characters
