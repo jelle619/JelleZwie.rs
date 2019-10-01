83c107794b35cba924b69c6f598d7c08bca06e9a
jelle619 <jelle619@users.noreply.github.com>
2019-10-01 14:42:33 +0000

    Automated deployment: Tue Oct  1 14:42:33 UTC 2019 80920332d87179c89d83ec9715cf3b4cf9e8493f

---
+++ script.js
@@ -0,0 +1,29 @@
+// Flying Pages async/defer
+window.addEventListener("load", () => {
+  flyingPages();
+});
+
+// Change cursor state during loading
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.body.style.cursor  = "wait";
+  window.onload=function(){document.body.style.cursor="default";};
+});
+
+// Luxon
+var localTime = luxon.DateTime.fromObject({zone: "Europe/Amsterdam"}).setLocale("en-gb").toLocaleString(luxon.DateTime.DATETIME_FULL);
+
+// Form enlarger
+document.addEventListener("DOMContentLoaded", function(event) {
+  function OnInput() {
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
+  }
+  var tx = document.getElementsByTagName("textarea");
+  for (var i = 0; i < tx.length; i++) {
+    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    tx[i].addEventListener("input", OnInput, false);
+  }
+});
+
+// Easter eggg
+console.info("Here be dragons! 🐲");
