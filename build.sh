2141adbdf969eaaf50f831de7e293ce21af0ad79
Jelle Z <jellezwiers@icloud.com>
2020-02-19 12:07:11 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,3 @@
 
 mkdocs build --clean --config-file mkdocs/characters/mkdocs.yml --site-dir ../../characters
 rm -rf mkdocs
-
-#  _   _
-# | | | | _____  _____
-# | |_| |/ _ \ \/ / _ \
-# |  _  |  __/>  < (_) |
-# |_| |_|\___/_/\_\___/
-
-# No build commands are needed at the moment.
