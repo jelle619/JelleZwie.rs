8418e41e71a3c43720c3b0008ab3e00788d0c791
Jelle Z <public@jellezwie.rs>
2020-09-15 17:18:11 +0200

    Import code form old site + hotfix

--- refresh/script.js
+++ refresh/script.js
@@ -1,3 +1,9 @@
+// Change cursor state during loading
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.body.style.cursor  = "wait";
+    window.onload=function(){document.body.style.cursor="default";};
+  });
+
 // Server Time
 
 var xmlHttp;
@@ -47,3 +53,20 @@ try {
 if (navigator.language.includes("nl")) {
     document.querySelector("#contact select[name=\"language\"]").value = "dutch";
 }
+
+// Automatic form enlarger
+document.addEventListener("DOMContentLoaded", function(event) {
+    function OnInput() {
+      this.style.height = "auto";
+      this.style.height = (this.scrollHeight) + "px";
+    }
+    var tx = document.getElementsByTagName("textarea");
+    for (var i = 0; i < tx.length; i++) {
+      tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+      tx[i].addEventListener("input", OnInput, false);
+    }
+  });
+
+// Because dragons are really cool
+
+console.info("Here be dragons! 🐲");
